fe254e199a8e38c9be30d412f306df01a05181c9
GartnerMor <GartnerMor@10.1.25.23>
2019-03-28 08:31:29 +0100

    Added Project „Lottozahlen“

--- Wetterstation/src/Wetterstation.java
+++ Wetterstation/src/Wetterstation.java
@@ -15,7 +15,7 @@ public class Wetterstation {
 
 		durchschnitt = summe / temp.length;
 		double gerunded = Math.round(durchschnitt * 100.0) / 100.0;
-		System.out.println("Der Durchschnitt beträgt " + gerunded + " C°.");
+		System.out.println("Der Durchschnitt beträgt ca. " + gerunded + " C°.");
 
 		int max = temp[0];
 		int min = temp[0];
@@ -29,37 +29,33 @@ public class Wetterstation {
 			}
 		}
 
-		System.out.println(max);
-		System.out.println(min);
-
+		System.out.println("Die höchste gemessene Temperatur ist: " + max);
+		System.out.println("Die niedrigste gemessene Temperatur ist: " + min);
 		System.out.println();
 
 		int indexTag = 0;
-		int differenz = 0;
-		for (int i = 0; i < temp.length; i++) {
+		int[] diff = new int[temp.length - 1];
 
-			if (i > 0) {
-
-				int heute = temp[i];
-				int gestern = temp[i - 1];
-				int tempDifferenz = 0;
-
-				if (heute > gestern) {
-					tempDifferenz = heute - gestern;
-				}
-				if (gestern > heute) {
-					tempDifferenz = gestern - heute;
-				}
-
-				if (tempDifferenz > differenz) {
-					differenz = tempDifferenz;
-					indexTag = i;
-				}
+		// Differenzen berechnen
+		for (int i = 0; i < diff.length; i++) {
+			if (temp[i] > temp[i + 1]) {
+				diff[i] = temp[i] - temp[i + 1];
+			} else {
+				diff[i] = temp[i + 1] - temp[i];
+			}			
+		}
+		
+		int maxDifferenz = diff[0];
+		int maxDifferenzIndex = 0;
+		for (int i = 0; i < diff.length; i++) {
+			if (diff[i] > maxDifferenz) {
+				maxDifferenz = diff[i];
+				maxDifferenzIndex = i;
 			}
 		}
 
-		System.out.println("Differenz: " + differenz + "C°");
-		System.out.println("Tag " + indexTag + "-" + (indexTag + 1));
+		System.out.println("Die größte Temperaturdifferenz beträgt: " + maxDifferenz);
+		System.out.println("Sie findet bei den Tagen " + maxDifferenzIndex + "-" + (maxDifferenzIndex + 1) + " statt.");
 
 	}
 }
