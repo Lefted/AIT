fd016427a8ad8d5662986408cfee0b78dd1e8ac4
GartnerMor <GartnerMor@C135-06.oes.ml-bw.de>
2019-04-04 09:21:22 +0200

    Added Project „BruteForceSortierAlgorythmus“

---
+++ BruteForceSortierAlgorythmus/src/BruteForceSortierAlgorythmus.java
@@ -0,0 +1,19 @@
+public class BruteForceSortierAlgorythmus {
+
+	public static void main(String[] args) {
+		//VARIABLEN
+		@SuppressWarnings("unused")
+		int[] zahlen = { 1, 4, 9, 10 };
+		
+	}
+	
+	//auf richtige reihenfolge überprüfen
+	@SuppressWarnings("unused")
+	private boolean richtigeReihenfolge(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			
+		}
+		
+		return true;
+	}
+}
