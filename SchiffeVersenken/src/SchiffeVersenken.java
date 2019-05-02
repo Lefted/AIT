cffa93f01b6022a816d93ca47b9ba1b774077176
Gartn <Gartn@DESKTOP-QULTD7D.fritz.box>
2019-05-02 15:53:12 +0200

    Methode „schiffAußerhalb“ überarbeitet

--- SchiffeVersenken/src/SchiffeVersenken.java
+++ SchiffeVersenken/src/SchiffeVersenken.java
@@ -5,20 +5,20 @@ import java.io.InputStreamReader;
 public class SchiffeVersenken {
 
 	public static void main(String[] args) throws IOException {
-		//VARIABLEN
+		// VARIABLEN
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		// spielfeld[x][y]
-		char[][] spielfeld = initSpielfeld();
+		char[][] spielfeld = spielfeldInitalisieren();
 
-		ausgabeSpielfeld(spielfeld);
+		spielfeldAusgeben(spielfeld);
 
-		//aufgabe 3
-		//5x : einlesen, überprüfen, schiff hinzufügen, feld ausgeben
-		eingabeSchiffe(spielfeld, reader);
+		// aufgabe 3
+		// 5x : einlesen, überprüfen, schiff hinzufügen, feld ausgeben
+		schiffeEinlesen(spielfeld, reader);
 	}
 
-	//spielfeld mit '.' initialisieren
-	private static char[][] initSpielfeld() {
+	// spielfeld mit '.' initialisieren
+	private static char[][] spielfeldInitalisieren() {
 		char[][] spielfeld = new char[10][10];
 
 		for (int i = 0; i < spielfeld.length; i++) {
@@ -29,8 +29,8 @@ public class SchiffeVersenken {
 		return spielfeld;
 	}
 
-	//spielfeld in der konsole ausgeben
-	private static void ausgabeSpielfeld(char[][] spielfeld) {
+	// spielfeld in der konsole ausgeben
+	private static void spielfeldAusgeben(char[][] spielfeld) {
 		System.out.println("  A B C D E F G H I J");
 
 		for (int i = 0; i < spielfeld.length; i++) {
@@ -43,100 +43,89 @@ public class SchiffeVersenken {
 		}
 	}
 
-	//aufgabe 3
-	//spieler schiffer platzieren lassen
-	private static void eingabeSchiffe(char[][] spielfeld, BufferedReader reader) throws IOException {
+	// aufgabe 3
+	// spieler schiffer platzieren lassen
+	private static void schiffeEinlesen(char[][] spielfeld, BufferedReader reader) throws IOException {
 		for (int i = 0; i < 5; i++) {
-			//länge deklarieren
-			int laenge;
+			// länge des schiffe setzen
+			int[] laengen = { 2, 3, 3, 4, 5 };
+			int laenge = laengen[i];
 
-			//länge der schiffe anhand des durchgangs setzen
-			switch (i + 1) {
-			case 1:
-				laenge = 2;
-				break;
-			case 2:
-				laenge = 3;
-				break;
-			case 3:
-				laenge = 3;
-				break;
-			case 4:
-				laenge = 4;
-				break;
-			case 5:
-				laenge = 5;
-				break;
-			default:
-				laenge = 2;
-				break;
-			}
-
-			//FORMAT
+			// FORMAT
 			System.out.println();
 
-			//RICHTUNG
-			System.out.print(
-					"In welcher Richtung soll das " + (i + 1) + ".Schiff (Länge = " + laenge + ") platziert werden? (H)orizontal/(V)ertikal: ");
+			// RICHTUNG
+			System.out.print("In welcher Richtung soll das " + (i + 1) + ".Schiff (Länge = " + laenge
+					+ ") platziert werden? (H)orizontal/(V)ertikal: ");
 			String eingabe = reader.readLine();
-			//in großbuchstaben umwandeln
+			// in großbuchstaben umwandeln
 			char richtung = Character.toUpperCase(eingabe.charAt(0));
 
-			//validen wert für richtung erzwingen
+			// validen wert für richtung erzwingen
 			while (!richtigeRichtung(richtung)) {
-				System.out.println("Ungültige Eingabe für Richtung!");
+				System.out.println("\"" + eingabe + "\"" + " ist keine gültige Eingabe für Richtung!");
 				System.out.print("Bitte erneut eingeben (H/V): ");
 				eingabe = reader.readLine();
 				richtung = Character.toUpperCase(eingabe.charAt(0));
 			}
 
-			//REIHE
-			System.out.print("In welcher Reihe soll der Anfang des " + (i + 1) + ".Schiffs (Länge = " + laenge + ") platziert werden? (0-9): ");
+			// REIHE
+			System.out.print("In welcher Reihe soll der Anfang des " + (i + 1) + ".Schiffs (Länge = " + laenge
+					+ ") platziert werden? (0-9): ");
 			eingabe = reader.readLine();
-			//in großbuchstaben umwandeln
+			// in großbuchstaben umwandeln
 			char reihe = Character.toUpperCase(eingabe.charAt(0));
 
-			//validen wert für reihe erzwingen
+			// validen wert für reihe erzwingen
 			while (!richtigeReihe(reihe)) {
-				System.out.println("Ungültige Eingabe für Reihe!");
+				System.out.println("\"" + eingabe + "\"" + " ist keine gültige Eingabe für Reihe!");
 				System.out.print("Bitte erneut eingeben (0-9): ");
 				eingabe = reader.readLine();
 				reihe = Character.toUpperCase(eingabe.charAt(0));
 			}
 
-			//SPALTE
-			System.out.print("In welcher Spalte soll der Anfang des " + (i + 1) + ". Schiffs (Länge = " + laenge + ") platziert werden? (A-J): ");
+			// SPALTE
+			System.out.print("In welcher Spalte soll der Anfang des " + (i + 1) + ". Schiffs (Länge = " + laenge
+					+ ") platziert werden? (A-J): ");
 			eingabe = reader.readLine();
-			//in großbuchstaben umwandeln
+			// in großbuchstaben umwandeln
 			char spalte = Character.toUpperCase(eingabe.charAt(0));
 
-			//validen wert für spalte erzwingen
+			// validen wert für spalte erzwingen
 			while (!richtigeSpalte(spalte)) {
-				System.out.println("Ungültige Eingabe für Spalte!");
+				System.out.println("\"" + eingabe + "\"" + " ist keine gültige Eingabe für Spalte!");
 				System.out.print("Bitte erneut eingeben (A-J): ");
 				eingabe = reader.readLine();
 				spalte = Character.toUpperCase(eingabe.charAt(0));
 			}
-			
-			//TODO
-			//sichergehen, dass Schiff nicht außerhalb des spielfelds geht
-			//sichergehen, dass Schiff nicht mit anderen Schiffen überlappt
 
-			//spielfeld[][] aktuelles schiff hinzufügen
-			ergaenzeSchiffe(spielfeld, richtung, reihe, spalte, laenge);
+			// TODO
+			// sichergehen, dass Schiff nicht außerhalb des spielfelds geht
+			// sichergehen, dass Schiff nicht mit anderen Schiffen überlappt
 
-			//FORAMT
+			if (!schiffAußerhalb(richtung, reihe, spalte, spielfeld, laenge)) {
+				// spielfeld[][] aktuelles schiff hinzufügen
+				System.out.println("g");
+				schiffHinzufuegen(spielfeld, richtung, reihe, spalte, laenge);
+			} else {
+				System.out.println();
+				System.err.println("Schiff liegt außerhalb des Spielfelds");
+				System.err.println("Shutting down system");
+				System.exit(-1);
+			}
+
+			// FORAMT
 			System.out.println();
 
-			//nach jeder eingabe spielfeld aktualisieren
-			ausgabeSpielfeld(spielfeld);
+			// nach jeder eingabe spielfeld aktualisieren
+			spielfeldAusgeben(spielfeld);
 		}
 
 	}
 
-	//spielfeld neue schiffe hinzufügen
-	private static void ergaenzeSchiffe(char[][] spielfeld, char richtung, char reihe, char spalte, int laenge) {
-		//spalte/reihe in koordinaten umwandeln
+	// spielfeld neue schiffe hinzufügen
+	private static void schiffHinzufuegen(char[][] spielfeld, char richtung, char reihe, char spalte, int laenge) {
+		// spalte/reihe in koordinaten umwandeln
 		int posX = spalte - 'A';
 		int posY = reihe - '0';
 
@@ -151,7 +140,7 @@ public class SchiffeVersenken {
 				posY++;
 			}
 		} else {
-			//fehler verarbeiten
+			// fehler verarbeiten
 			System.out.println();
 			System.err.println("Variable 'richtung' has an invalid value: " + richtung);
 			System.out.println("Exiting program");
@@ -159,16 +148,17 @@ public class SchiffeVersenken {
 		}
 	}
 
-	//auf richtige spalte testen
+	// auf richtige spalte testen
 	private static boolean richtigeSpalte(char spalte) {
-		if (!((spalte == 'A') || (spalte == 'B') || (spalte == 'C') || (spalte == 'D') || (spalte == 'E') || (spalte == 'F') || (spalte == 'G')
-				|| (spalte == 'H') || (spalte == 'I') || (spalte == 'J'))) {
+		final int decWert = spalte;
+
+		if (!(('A' <= decWert) && (decWert <= 'J'))) {
 			return false;
 		}
 		return true;
 	}
 
-	//auf richtige richtung testen
+	// auf richtige richtung testen
 	private static boolean richtigeRichtung(char richtung) {
 		if ((richtung != 'H') && (richtung != 'V')) {
 			return false;
@@ -176,11 +166,34 @@ public class SchiffeVersenken {
 		return true;
 	}
 
-	//auf richtige reihe testen
+	// auf richtige reihe testen
 	private static boolean richtigeReihe(char reihe) {
 		if (!((0 <= Character.getNumericValue(reihe)) && (Character.getNumericValue(reihe) < 10))) {
 			return false;
 		}
 		return true;
+	}
+
+	// testen ob schiff außerhalb des spielfeld ist
+	private static boolean schiffAußerhalb(char richtung, char reihe, char spalte, char[][] spielfeld, int laenge) {
+		int posX = spalte - 'A';
+		int posY = reihe - '0';
+		
+		if (richtung == 'H') {
+			for (int i = 0; i < laenge; i++) {
+				posX++;
+				if (posX >= spielfeld.length) {
+					return true;
+				}
+			}
+		} else if (richtung == 'V') {
+			for (int i = 0; i < laenge; i++) {
+				posY++;
+				if (posY >= spielfeld[0].length) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
